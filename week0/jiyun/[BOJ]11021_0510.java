df6211423dbef97d86e6d12c16080c38406a70ab
Ryujy <63836145+Ryujy@users.noreply.github.com>
2023-05-10 06:47:53 +0900

    BufferedWriter를 사용한 코드로 업데이트
    BufferedWriter를 사용하면 시간이 144ms -> 124ms로 줄어든다.

--- week0/jiyun/[BOJ]11021_0510.java
+++ week0/jiyun/[BOJ]11021_0510.java
@@ -1,24 +1,28 @@
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // BufferedWriter를 사용하면 시간이 더 줄어든다. 144ms -> 124ms (java 11 기준)
+        // BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int T = Integer.parseInt(br.readLine());
 
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
 
-        for(int i = 0; i < T; i++){
+        for(int i = 1; i <= T; i++){
             st = new StringTokenizer(br.readLine(), " ");
-            int num = i+1;
-            sb.append("Case #"+num+": ");
-            sb.append(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()) );
-            sb.append('\n');
+            // .append의 새로운 작성법을 배움
+            sb.append("Case #")
+                .append(i)
+                .append(": ")
+                .append(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()) )
+                .append('\n');
         }
         System.out.println(sb);
+        // bw.write(sb.toString());
+        // bw.flush();
 
     }
 }
