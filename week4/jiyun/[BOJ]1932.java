f2b2bf791d5f375014118daa86b9763f93dd1ab5
Ryujy <63836145+Ryujy@users.noreply.github.com>
2023-06-15 12:30:12 +0900

    Update [BOJ]1932.java

--- week4/jiyun/[BOJ]1932.java
+++ week4/jiyun/[BOJ]1932.java
@@ -5,8 +5,8 @@ import java.util.StringTokenizer;
  
 public class Main {
  
-	static int[][] arr;
-	static Integer[][] dp;
+	static int[][] arr; // 입력되는 정수 삼각형을 저장할 2차원 배열
+	static Integer[][] dp; // 계산한 최댓값을 저장할 2차원 배열 (memoization)
 	static int N;
  
 	public static void main(String[] args) throws IOException {
@@ -18,15 +18,15 @@ public class Main {
 		arr = new int[N][N];
 		dp = new Integer[N][N];
 		StringTokenizer st;
-		for (int i = 0; i < N; i++) {
+		for (int i = 0; i < N; i++) { // 정수삼각형 depth
 			st = new StringTokenizer(br.readLine(), " ");
  
-			for (int j = 0; j < i + 1; j++) {
+			for (int j = 0; j < i + 1; j++) { //정수 삼각형 idx
 				arr[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
  
-		for (int i = 0; i < N; i++) {
+		for (int i = 0; i < N; i++) { // 가장 마지막 행은 기저값으로, arr와 동일한 값으로 채워준다.
 			dp[N - 1][i] = arr[N - 1][i];
 		}
  
@@ -35,10 +35,11 @@ public class Main {
 	}
  
 	static int find(int depth, int idx) {
-		if(depth == N - 1) return dp[depth][idx];
+		if(depth == N - 1) return dp[depth][idx]; // depth가 마지막 행일 경우에는 바로 값을 반환해준다. (이미 채워져있으므로)
  
-		if (dp[depth][idx] == null) {
-			dp[depth][idx] = Math.max(find(depth + 1, idx), find(depth + 1, idx + 1)) + arr[depth][idx];
+		if (dp[depth][idx] == null) { // dp는 마지막 행 외에 다 null 이므로 0,0에서 시작 -> 밑에서 두번째까지 내려감
+			// 다음행의 왼쪽,오른쪽값 중의 최댓값 + 해당 자리의 정수 => dp에 최댓값 메모
+			dp[depth][idx] = Math.max(find(depth + 1, idx), find(depth + 1, idx + 1)) + arr[depth][idx]; 
 		}
 		return dp[depth][idx];
  
