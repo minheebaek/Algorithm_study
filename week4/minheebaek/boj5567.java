c7785f9a9b269a2123ab403f6d053f90cb22f71e
minhee baek <bsmq100@gmail.com>
2023-06-18 03:03:02 +0900

    boj 5567

--- week4/minheebaek/boj5567.java
+++ week4/minheebaek/boj5567.java
@@ -1,50 +1,60 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main{
+    public static int ans=0;
     public static ArrayList<Integer>[] list;
     public static int[] visited;
-    public static int cnt=0;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int m = Integer.parseInt(br.readLine());
 
-        list = new ArrayList[n+1];
-        visited = new int[n+1];
-
-        for(int i=1; i<= n; i++){
-            list[i]=new ArrayList<Integer>();
-
+        list = new ArrayList[n+1];//동기 배열 만들고
+        visited = new int[n+1];//방문도 만들어야함
+        //배열 안에 또 ArrayList
+        for(int i=1; i<n+1; i++){
+            list[i]=new ArrayList<>();
         }
+
+        //양방향 그래프 설정
         for(int i=0; i<m; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
+
             list[a].add(b);
             list[b].add(a);
+
         }
         bfs();
-        System.out.println(cnt);
+        System.out.println(ans);
+
     }
     public static void bfs(){
-        Queue<Integer> q = new LinkedList<>();
-        q.add(1);
-        visited[1]++;
+        //bfs 사용을 위한 큐 선언 LinkedList 사용
+        Queue<Integer> queue = new LinkedList<>();
+        //처음 원소 큐에 넣음
+        queue.add(1);
+        visited[1]++; //방문처리
 
-        while(!q.isEmpty()){
-            int v = q.poll();
-
-            if(visited[v] >=3){
+        while(!queue.isEmpty()){ //큐가 다 비워질때까지 반복
+            int v=queue.poll(); //큐 뽑아냄
+            if(visited[v]>=3){ //3이상이면 친구의 친구까지 다 돌았다는거니까
                 break;
             }
-            for(int next : list[v]){
+            for(int next : list[v]){ //그게 아니라면 뽑은 원소를 기준으로
+                //인접노드들을 하나씩 확인함
                 if(visited[next]==0){
+                    //아직 방문하지 않았다면
                     visited[next]=visited[v]+1;
-                    q.add(next);
-                    cnt++;
+                    //방문처리하고
+                    queue.add(next);
+                    ans++;
                 }
+
             }
         }
+
     }
 }
