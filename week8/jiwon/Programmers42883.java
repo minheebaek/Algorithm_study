a2dfa0bf08ab6f209555506ac75d618c6df03236
kiwijomn <116738827+kiwijomn@users.noreply.github.com>
2023-07-15 03:09:31 +0900

    Programmers42883.java

---
+++ week8/jiwon/Programmers42883.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String solution(String number, int k) {
+        StringBuilder answer = new StringBuilder();
+        
+        int cnt = 0;
+		char max;
+
+		for (int i = 0; i < number.length() - k; i++) {	
+			max = '0';
+			for (int j = cnt; j <= k + i; j++) {	
+				if (number.charAt(j) > max) {
+					max = number.charAt(j);	
+					cnt = j + 1;
+				}
+			}
+			answer.append(max);
+		}
+        return answer.toString();
+    }
+}
